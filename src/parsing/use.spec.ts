174cd255ee331431b11a7fe15b544da9ebb21186
Anthony Johnston <aj@antix.co.uk>
2022-11-26 17:56:00 +0000

    test(parsing): use, more coverage

--- src/parsing/use.spec.ts
+++ src/parsing/use.spec.ts
@@ -11,7 +11,6 @@ describe('use', () => {
     const result = parser.parse(null);
 
     expect(result.errors).toEqual(ParseErrors.required);
-
   });
 
   it('replaced errors', () => {
@@ -22,7 +21,6 @@ describe('use', () => {
     const result = parser.parse(null);
 
     expect(result.errors).toEqual(errors);
-
   });
 
   it('wrapped errors', () => {
@@ -33,7 +31,32 @@ describe('use', () => {
     const result = parser.parse(null);
 
     expect(result.errors).toEqual(errors);
-
   });
 
-})
+  describe('add rule parser', () => {
+    const length = 2;
+    const intParser = Is
+      .string
+      .minLength(length)
+      .maxLength(length);
+
+    it('make required', () => {
+
+      const requiredIntParser = Is.required.use(intParser);
+      const result = requiredIntParser.parse(undefined);
+
+      expect(result.errors).toEqual(ParseErrors.required);
+    });
+
+    it('as property', () => {
+
+      const objectParser = Is.object({
+        id: Is.required.use(intParser)
+      });
+
+      const result = objectParser.parse({ id: null });
+
+      expect(result.errors.id).toEqual(ParseErrors.required);
+    });
+  });
+});
